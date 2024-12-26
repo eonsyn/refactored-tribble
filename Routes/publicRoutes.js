0dde45b9038d3f73fd473d5b2761c561d22ca751
Aryan Singh <aryan03aryansingh@gmail.com>
2024-12-26 16:51:35 +0530

    code update

--- Routes/publicRoutes.js
+++ Routes/publicRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
+// const axios = require("axios");
 const jsdom = require("jsdom");
 require("dotenv").config();
 const authenticateAdmin = require("../Middelware/admin.auth.midddleware");
