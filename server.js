fbd36216035764432e85bdc03439bc4b1e269976
eonsyn <154956467+eonsyn@users.noreply.github.com>
2024-12-26 19:01:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const adminRoute = require("./Routes/adminRoute.js");
 const app = express();
 connectdb();
 const corsConfig = {
-  origin: ["https://moodyfilm.netlify.app"],
+  origin: ["https://moodyfilm.netlify.app"," http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
