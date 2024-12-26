f08b98fa65bab1f6c05329d0fbffa18aee6ff9b9
Aryan Singh <aryan03aryansingh@gmail.com>
2024-12-26 16:52:46 +0530

    code update

--- Routes/publicRoutes.js
+++ Routes/publicRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 // const axios = require("axios");
-const jsdom = require("jsdom");
+// const jsdom = require("jsdom");
 require("dotenv").config();
 const authenticateAdmin = require("../Middelware/admin.auth.midddleware");
 const RequestFilm = require("../models/RequestFilm");
-const { JSDOM } = jsdom;
+// const { JSDOM } = jsdom;
 const Film = require("../Models/Films"); // Replace with the actual path to your Film model
 
 router.get("/test", (req, res) => {
