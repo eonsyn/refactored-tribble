6e4be7f2af535dd891fae0d288354c29f7db5043
Aryan Singh <aryan03aryansingh@gmail.com>
2024-12-28 23:36:18 +0530

    Add api /getImdb

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -7,6 +7,12 @@ const bcrypt = require("bcrypt");
 const authenticateAdmin = require("../Middelware/admin.auth.midddleware");
 const Film = require("../Models/Films");
 const axios = require("axios");
+
+const extractTitleId = (url) => {
+  const match = url.match(/\/title\/(tt\d+)\//);
+  return match ? match[1] : null;
+};
+
 router.post("/login", async (req, res) => {
   try {
     const { userId, password } = req.body;
@@ -83,7 +89,160 @@ router.post("/login", async (req, res) => {
 
 // POST it take id and update the save the which is in the  database
 
-router.post("/getImdbData", (req, res) => {});
+router.post("/getImdbData", async (req, res) => {
+  const { url } = req.body; // Access URL from the request body
+
+  if (!url) {
+    return res
+      .status(400)
+      .send({ error: "URL is required in the request body." });
+  }
+  const titleId = extractTitleId(url);
+  const storyUrl = `https://caching.graphql.imdb.com/?operationName=TMD_Storyline&variables=${encodeURIComponent(
+    JSON.stringify({ locale: "en-US", titleId })
+  )}&extensions=${encodeURIComponent(
+    JSON.stringify({
+      persistedQuery: {
+        sha256Hash:
+          "78f137c28457417c10cf92a79976e54a65f8707bfc4fd1ad035da881ee5eaac6",
+        version: 1,
+      },
+    })
+  )}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,hi;q=0.6",
+        "Cache-Control": "max-age=0",
+        Cookie: "_lscache_vary=00e02ac3526ebf42934719326cc549fc",
+        DNT: "1",
+        Referer: "https://www.imdb.com/", // Referer updated to match the target URL
+        "Sec-CH-UA":
+          '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "Sec-CH-UA-Mobile": "?0",
+        "Sec-CH-UA-Platform": '"Android"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent":
+          "Mozilla/5.0 (Linux; Android) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.109 Safari/537.36 CrKey/1.54.248666",
+      },
+      responseType: "arraybuffer",
+    });
+
+    // Decode the HTML response
+    const decodedHtml = Buffer.from(response.data, "binary").toString("utf8");
+
+    // Write the HTML content to 'index.html'
+    // const filePath = path.join(__dirname, "index.html");
+    // fs.writeFileSync(filePath, decodedHtml, "utf8");
+
+    // console.log(`HTML content written to ${filePath}`);
+
+    // Parse the HTML with JSDOM
+    const dom = new JSDOM(decodedHtml);
+    const doc = dom.window.document;
+    //finding the rating of the film
+    const ratingElement = doc.querySelector(".imUuxf");
+    const rating = ratingElement ? ratingElement.textContent : null;
+
+    //find director
+    const directorElement = doc.querySelector(
+      ".ipc-metadata-list-item__list-content-item"
+    );
+    const director = directorElement ? directorElement.textContent : null;
+
+    //find film name
+    const filmnameElement = doc.querySelector(".hero__primary-text");
+    const filmname = filmnameElement ? filmnameElement.textContent : null;
+
+    // Find the image with class "ipc-image" and get its src attribute
+    const imageElement = doc.querySelector(".ipc-image");
+    const imageSrc = imageElement ? imageElement.src : null;
+
+    if (imageSrc) {
+      // Retain the '@._V1' part of the URL
+      const cleanedImageSrc = imageSrc.includes("@")
+        ? imageSrc.split("@")[0] + "@._V1.jpg"
+        : imageSrc;
+      var storySummary = "";
+      const genres = [];
+      try {
+        // Make the API request
+        const response = await axios.get(storyUrl, {
+          headers: {
+            Accept: "application/graphql+json, application/json",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language":
+              "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,hi;q=0.6",
+            "Content-Type": "application/json",
+            Cookie:
+              "uu=eyJpZCI6InV1MjRhNTM4ZDM5NGE3NGY2Mjg5ZDciLCJwcmVmZXJlbmNlcyI6eyJmaW5kX2luY2x1ZGVfYWR1bHQiOmZhbHNlfX0=; session-id=146-3613307-4397922; session-id-time=2082787201l; ubid-main=132-2409063-9532842; ad-oo=0; session-token=twUthgcheQIkMTir4V52DVdtwcFyBIy4vdbaOm9QUKL4X5xX41Lep9ji4K2OSBM91odGPY4OX6TjY3EGHWjj3jHe7RCldxNKF4ZnYvwJ/5xFupbWIrukbRTn0Tz2J49s7xsKq2P0Xa008jAaK3ZfREVUj6P49OuKs3URhoJyCALk5b2BWecNRWW/tlktuy6byVx0C+m6OJobD1a3ljXGTo/2PYpPgjgNIjFeo0QzRS5cJVdT3k0doj2tTsNCbn8+GFF2LJ6NY8tmSGllOJt61ZI6Sfv5y5JTlvgmqLhuQIqCQgKdYUTyqepT420ilVHDxxvZ1WKbWd8ASryClWx7GbnpSiQ2rwMY; ci=e30",
+            DNT: "1",
+            Origin: "https://m.imdb.com",
+            Referer: "https://m.imdb.com/",
+            "Sec-CH-UA":
+              '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+            "Sec-CH-UA-Mobile": "?1",
+            "Sec-CH-UA-Platform": '"Android"',
+            "User-Agent":
+              "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36",
+            "X-Amzn-Sessionid": "146-3613307-4397922",
+            "X-IMDB-Client-Name": "imdb-web-next-localized",
+            "X-IMDB-Client-Rid": "FFYVFXMSCTBS6MK651T2",
+            "X-IMDB-User-Country": "US",
+            "X-IMDB-User-Language": "en-US",
+            "X-IMDB-Weblab-Treatment-Overrides": "{}",
+          },
+          responseType: "arraybuffer",
+        });
+
+        // Decode the response buffer into a string
+        const decodedResponse = Buffer.from(response.data).toString("utf-8");
+
+        // Parse the string into JSON (if the API returns JSON data)
+        const parsedResponse = JSON.parse(decodedResponse);
+
+        // Log the parsed response to the console
+
+        storySummary =
+          parsedResponse.data.title.summaries.edges[0].node.plotText.plaidHtml;
+        const genress = parsedResponse.data.title.genres.genres;
+
+        genress.forEach((element) => {
+          genres.push(element.id);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
+      res.send({
+        titleId,
+        posterImg: cleanedImageSrc,
+        rating,
+        genres,
+        filmname,
+        director,
+        storySummary,
+      });
+    } else {
+      res.status(404).send({
+        error: "Image not found.",
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching or processing the webpage:", error.message);
+    res
+      .status(500)
+      .send({ error: "An error occurred while processing the request." });
+  }
+});
 
 router.post("/sendFormData", authenticateAdmin, async (req, res) => {
   try {
@@ -112,8 +271,15 @@ router.post("/sendFormData", authenticateAdmin, async (req, res) => {
     // Create a new film document
     const newFilm = new Film({
       filmTitle,
+      downloadData, // Add downloadData if required in the Film schema
+      imageData, // Add imageData if required in the Film schema
+      description, // Add description if required in the Film schema
+      imdbRating, // Add imdbRating if required in the Film schema
+      directedBy, // Add directedBy if required in the Film schema
+      releaseDate, // Add releaseDate if required in the Film schema
+      genre, // Add genre if required in the Film schema
       urlOfThumbnail,
-      urlOfPost, // Added urlOfPost here
+      urlOfPost,
     });
 
     // Save the film document to the database
@@ -121,7 +287,7 @@ router.post("/sendFormData", authenticateAdmin, async (req, res) => {
 
     // Send the ID of the new film to update other data if needed
     const finalResponse = await axios.post(
-      `${process.env.BACKENED_URL}/api/updateData`,
+      `${process.env.BACKEND_URL}/api/updateData`, // Fixed typo
       { id: newFilm._id.toString() } // Ensure the ID is being passed correctly
     );
 
